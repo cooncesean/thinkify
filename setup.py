8d5972c78ddd0b91e43a8369a706f48563346f96
cooncesean <cooncesean@gmail.com>
2013-07-20 09:28:49 -0700

    Adding author info to setup.py

--- setup.py
+++ setup.py
@@ -7,10 +7,12 @@ install_requires = [
 setup(
     name='Thinkify',
     description='A Python wrapper for the Thinkify Reader Protocol.'
-    version='0.1',
+    version='0.1.0',
+    author='Sean Coonce',
+    author_email='cooncesean@gmail.com',
     packages=['thinkify',],
     url='https://github.com/cooncesean/thinkify',
-    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    license='LICENSE.txt',
     long_description=open('README.md').read(),
     install_requires=install_requires,
 )
