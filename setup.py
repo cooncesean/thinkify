a391fef5bd0e9750f6b7191f8f37aaa24909e0de
cooncesean <cooncesean@gmail.com>
2013-07-20 10:50:16 -0700

    more versioning patches

--- setup.py
+++ setup.py
@@ -1,21 +1,15 @@
-import os
-
 from distutils.core import setup
-
-f = open(os.path.join(os.path.dirname(__file__), 'README.rst'))
-long_description = f.read()
-f.close()
 
 setup(
     name='thinkify',
     description='A Python wrapper for the Thinkify Reader Protocol.',
-    version='0.1.3',
+    version='0.1.5',
     author='Sean Coonce',
     author_email='cooncesean@gmail.com',
     packages=['thinkify',],
     url='https://github.com/cooncesean/thinkify',
     license='LICENSE.txt',
-    long_description=long_description,
+    long_description='More info can be found at: http://github.com/cooncesean/thinkify',
     install_requires=[
     	'pyserial>=2.6',
 	],
